0a5371f83a2a4e43669dfc9a94240c8dbdbe6aca
cHurm16 <chloe.hurm@stu.daviess.kyschools.us>
2025-04-17 12:29:47 +0000

    homework

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,28 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		//return -1 if it is not there 
+		int start = 0;
+		int end= ray.length-1;
+		
 
+		while(start<=end){
+			//find the middle value 
+			//int mid = (start+end)
+
+			int mid = start+((end-start)/2);
+
+			if(<ray[mid]){
+				end = mid-1;
+			}
+			else if (>ray[mid]){
+				start = mid+1;
+			}
+			else {
+				//answer was found 
+				return mid; 
+			}
+		}
+			return -1;
 	}
 }
